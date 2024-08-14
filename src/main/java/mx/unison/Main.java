a6b4512a334a6dd008cdc7db93947f60d5c8ab55
josue <a223316771@uniso.mx>
2024-08-14 14:05:14 -0700

    hola

--- src/main/java/mx/unison/Main.java
+++ src/main/java/mx/unison/Main.java
@@ -4,9 +4,42 @@ package mx.unison;
 y cantos a asentamientos rurales
  */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        String archivo = "codigos_postales.csv";
+        String linea;
+        String separador = ",";
+        int contadorUrbano = 0;
+        int contadorRural = 0;
 
-        System.out.println("Hello world!");
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+
+            while ((linea = br.readLine()) != null) {
+
+                String[] partes = linea.split(separador);
+
+
+                if (partes.length >= 3) {
+                    String tipoAsentamiento = partes[2].trim();
+
+                    // Contar el tipo de asentamiento
+                    if (tipoAsentamiento.equalsIgnoreCase("Urbano")) {
+                        contadorUrbano++;
+                    } else if (tipoAsentamiento.equalsIgnoreCase("Rural")) {
+                        contadorRural++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Imprimir resultados
+        System.out.println("Número de asentamientos urbanos: " + contadorUrbano);
+        System.out.println("Número de asentamientos rurales: " + contadorRural);
     }
 }
